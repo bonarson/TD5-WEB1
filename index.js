86ad018a3de2855e1c7381e075001e1ffbb9737d
bonarson <hei.bonarson@gmail.com>
2023-05-01 23:00:50 +0300

    voici mon fichier sur le (increment)

---
+++ index.js
@@ -0,0 +1,15 @@
+var number = document.getElementById('number');
+var button = document.getElementById('increment');
+
+
+var x = 0;
+function incri() {
+    x += 1;
+    number.innerText = `count: ${x}`;
+}
+
+
+
+
+
+
